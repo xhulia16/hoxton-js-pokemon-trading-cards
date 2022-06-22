488f6b2fe2aaae84d6319495270c117833232592
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-22 22:52:30 +0200

    created elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // - Make sure you check and understand the data that is given to you!
 // - Create a card using JS that represents a single pokemon, use the example image as a reference. You will also find an HTML example commented out in the index.html
 // - Use the exact CSS classes you see in the example HTML to obtain the same style for each card
@@ -7,4 +8,34 @@
 
 // - Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
 
-console.log(data);
+//<li class="card">
+//<h2 class="card--title">Bulbasaur//</h2>
+//<img
+//width="256"
+//class="card--img"
+//src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+///>
+//<ul class="card--text">
+//<li>HP: 45//</li>
+//<li>ATTACK: 49//</li>
+//<li>DEFENSE: 49//</li>
+//<li>SPECIAL-ATTACK: 65//</li>
+//<li>SPECIAL-DEFENSE: 65//</li>
+//<li>SPEED: 45//</li>
+//</ul>
+//</li>
+function createPokemonCards(pokemon) {
+
+    let cardLi = document.createElement("li")
+
+    let titleH2 = document.createElement("h2")
+    let pokemonImg = document.createElement("img")
+    let statsUl = document.createElement("ul")
+
+    let hpLi = document.createElement("li")
+    let attackLi = document.createElement("li")
+    let defenseLi = document.createElement("li")
+    let specialAttackLi = document.createElement("li")
+    let specialDefenseLi = document.createElement("li")
+    let speedLi = document.createElement("li")
+}
