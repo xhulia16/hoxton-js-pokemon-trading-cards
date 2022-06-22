ec92b7e7daf87bd233de2cbf93d36137e6faf459
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-23 00:15:41 +0200

    one pokemon card

--- src/index.js
+++ src/index.js
@@ -27,15 +27,45 @@
 function createPokemonCards(pokemon) {
 
     let cardLi = document.createElement("li")
+    cardLi.className='card'
 
     let titleH2 = document.createElement("h2")
+    titleH2.className='card--tittle'
+    titleH2.textContent='Pokemon name'
+
     let pokemonImg = document.createElement("img")
+    pokemonImg.className='card--img'
+    pokemonImg.width=256
+    pokemonImg.src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+    
     let statsUl = document.createElement("ul")
+    statsUl.className='card--text'
 
     let hpLi = document.createElement("li")
+    hpLi.textContent='HP: 45'
+
     let attackLi = document.createElement("li")
+    attackLi.textContent='ATTACK: 49'
+
     let defenseLi = document.createElement("li")
+    defenseLi.textContent='DEFENSE: 49'
+
     let specialAttackLi = document.createElement("li")
+    specialAttackLi.textContent='SPECIAL-ATTACK: 65'
+
     let specialDefenseLi = document.createElement("li")
+    specialDefenseLi.textContent='SPECIAL-DEFENSE: 65'
+
     let speedLi = document.createElement("li")
+    speedLi.textContent='SPEED: 45'
+
+
+    cardLi.append(titleH2, pokemonImg, statsUl)
+    statsUl.append(hpLi, attackLi, defenseLi, specialAttackLi, specialDefenseLi, speedLi)
+
+    console.log(cardLi)
+
+    let cardsUl = document.querySelector(".cards")
+    cardsUl.append(cardLi)
 }
+createPokemonCards()
