b633aeab46ea5a295500dd44a33403cabbd47391
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-22 20:32:23 +0200

    data file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// - Make sure you check and understand the data that is given to you!
+// - Create a card using JS that represents a single pokemon, use the example image as a reference. You will also find an HTML example commented out in the index.html
+// - Use the exact CSS classes you see in the example HTML to obtain the same style for each card
+// - The cards should be nested inside <ul class="cards"></ul>
+// - Use the official-artwork object key as the images for the card. The images are all inside of the sprites key, in each pokemon object
+// pokemon.sprites.other['official-artwork'].front_default
+
+// - Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
+
+console.log(data);
